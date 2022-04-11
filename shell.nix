b0a64b861a6064eed403775cbfe66adac0826217
Henrik Lissner <git@henrik.io>
2022-04-12 01:26:44 +0200

    nit: remove unused PATH entry in shell.nix

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
     mkdir -p .gem
     export GEM_HOME=$PWD/.gem
     export GEM_PATH=$GEM_HOME
-    export PATH=$PWD/bin:$GEM_HOME/bin:$PATH
+    export PATH=$GEM_HOME/bin:$PATH
 
     if ! command -v discourse_theme >/dev/null; then
       echo "Installing discourse_theme CLI"
